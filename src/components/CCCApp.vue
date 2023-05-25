dd573a00e6a2fae374653c8e1ad654fd5195a044
Erik Verheul <tech@verheulconsultants.nl>
2023-05-25 22:09:50 +0200

    Some finetuning

--- src/components/CCCApp.vue
+++ src/components/CCCApp.vue
@@ -1,34 +1,37 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
-      <app-bar :is-authenticated="matchGevonden"></app-bar>
-      <base-card>
-        <form @submit.prevent="submitForm">
-          <div class="form-control">
-            <template v-if="userEntryMode === 'login'">
-              <label for="schuilNaam">Uw schuilnaam</label>
-              <input type="text" id="schuilNaam" v-model.trim="schuilNaam" />
-            </template>
-            <template v-if="userEntryMode === 'signup'">
-              <select-alias 
-                :assigned-aliases="assignedAliases" 
-                :all-aliases="allAliases"
-                @alias-selected="showSelectedAlias">
-              </select-alias>
-              <p v-if="selectedAlias !== undefined">Gekozen schuilnaam is {{ selectedAlias }}</p>
-            </template>
-          </div>
-          <div class="form-control">
-            <label v-if="userEntryMode === 'login'" for="passwordId">Wachtwoord</label>
-            <label v-if="userEntryMode === 'signup'" for="passwordId">Kies een wachtwoord van 6 of meer tekens</label>
-            <input type="password" id="passwordId" v-model.trim="password" />
-          </div>
-          <p v-if="!formIsValid">Schuilnaam en wachtwoord combinatie onjuist.</p>
-          <base-button>{{ submitButtonCaption }}</base-button>
-          <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
-        </form>
-      </base-card>
+      <app-bar :is-authenticated="isAuthenticated"></app-bar>
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+          <v-card variant="text">
+            <v-form @submit.prevent>
+              <div>
+                <template v-if="userEntryMode === 'login'">
+                  <v-card-title class="text-h5">login</v-card-title>
+                  <v-card-subtitle>Login met uw alias en PIN code</v-card-subtitle>
+                  <v-text-field v-model.trim="alias" label="Uw alias" :rules="nameRules" />
+                </template>
+                <template v-if="userEntryMode === 'signup'">
+                  <select-alias :assigned-aliases="assignedAliases" :all-aliases="allAliases"
+                    @alias-selected="setSelectedAlias">
+                  </select-alias>
+                  <div class="py-2" />
+                  <label>Kies een PIN code</label>
+                </template>
+                <v-text-field v-model.trim="PIN" label="PIN" :rules="pinRules" />
+              </div>
+              <v-btn v-if="userEntryMode === 'login'" type="submit" color="black" @click='doSigninUser' rounded="l"
+                size="large">Login</v-btn>
+              <v-btn v-if="userEntryMode === 'signup'" type="submit" color="black" @click='doSignupUser' rounded="l"
+                size="large">Maak persoonlijke alias aan</v-btn>
+              <v-btn type="button" variant="text" @click="switchAuthMode">{{ switchModeButtonCaption }}</v-btn>
+            </v-form>
+          </v-card>
+        </v-col>
+      </v-row>
 
+      <div class="py-14" />
 
       <v-img height="300" src="@/assets/logo.svg" />
 
@@ -75,26 +78,45 @@ import AppBar from './AppBar.vue'
 export default {
   data() {
     return {
+      userEntryMode: 'login',
       selectedAlias: undefined,
+      formIsValid: false,
+      alias: '',
+      nameRules: [
+        value => {
+          if (this.assignedAliases.includes(value)) return true
 
-      schuilNaam: '',
-      matchGevonden: false,
+          return 'Alias onbekend.'
+        },
+      ],
+      PIN: '',
+      pinRules: [
+        value => {
+          if (value) return true
+
+          return 'Vul 4 of meer cijfers in.'
+        },
+        value => {
+          if (!isNaN(value)) return true
+
+          return 'Vul alleen cijfers in.'
+        },
+        value => {
+          if (value.length >= 4) return true
+
+          return 'Vul minimaal 4 cijfers in.'
+        },
+      ],
+      isAuthenticated: false,
       allAliases: ['Alain', 'Alberto', 'Aldo', 'Alessandro', 'Alexander', 'Alfred', 'Alice', 'Alla', 'Anastasiya', 'Anatoliy', 'Andre', 'Andrea', 'Andreas', 'Andrew', 'Andriy', 'Angelo', 'Anne', 'Anthony', 'Antonio', 'Armando', 'Arthur', 'Bartolomeo', 'Bas', 'Benedetto', 'Bernard', 'Bernd', 'Bernhard', 'Bjorn', 'Bohdan', 'Bram', 'Brian', 'Bruno', 'Carl', 'Carlo', 'Caroline', 'Cas', 'Catherine', 'Charles', 'Christian', 'Christine', 'Christopher', 'Claude', 'Claudio', 'Corinne', 'Corrado', 'Daan', 'Daniel', 'Daniele', 'David', 'Davide', 'Denis', 'Dennis', 'Dieter', 'Dirk', 'Dmytro', 'Domenico', 'Donald', 'Douglas', 'Dylan', 'Edward', 'Emanuele', 'Emiliano', 'Emilio', 'Emmanuel', 'Enrico', 'Eric', 'Erich', 'Ernst', 'Fabio', 'Fabrice', 'Federico', 'Filippo', 'Florence', 'Floris', 'Francesco', 'Frank', 'Franz', 'Frederic', 'Freek', 'Fritz', 'Gabriele', 'Gaetano', 'Gary', 'Georg', 'George', 'Gerard', 'Giacomo', 'Gianni', 'Gijs', 'Gilbert', 'Giorgio', 'Giovanni', 'Giuseppe', 'Gregory', 'Günter', 'Gustav', 'Halyna', 'Hanna', 'Hans', 'Hans-Peter', 'Harold', 'Heinz', 'Helmut', 'Hendrik', 'Henk', 'Henry', 'Hermann', 'Horst', 'Ihor', 'Ingo', 'Inna', 'Iryna', 'Isabelle', 'Ivan', 'Jacob', 'Jacques', 'James', 'Jan', 'Jarno', 'Jason', 'Jean', 'Jeffrey', 'Jelle', 'Jelte', 'Jerome', 'Jerry', 'Jesse', 'Joachim', 'Joep', 'Johannes', 'John', 'Joost', 'Joris', 'Jose', 'Josef', 'Joseph', 'Joshua', 'Julie', 'Jürgen', 'Karl', 'Kateryna', 'Kenneth', 'Kevin', 'Khrystyna', 'Klaas', 'Klaus', 'Kurt', 'Larry', 'Lars', 'Larysa', 'Laurence', 'Laurens', 'Lennard', 'Lothar', 'Luc', 'Luca', 'Lucas', 'Luciano', 'Luigi', 'Lyubov', 'Lyudmyla', 'Maarten', 'Manfred', 'Marc', 'Marcello', 'Marco', 'Marie', 'Mario', 'Mariya', 'Mark', 'Markus', 'Martijn', 'Martin', 'Martine', 'Mary', 'Maryna', 'Massimo', 'Matthew', 'Matthias', 'Matthijs', 'Max', 'Michael', 'Michel', 'Milan', 'Monique', 'Mykhailo', 'Mykola', 'Myroslav', 'Nadiya', 'Nataliya', 'Nathalie', 'Nicolas', 'Nicolo', 'Niels', 'Oksana', 'Oleh', 'Oleksandr', 'Oleksiy', 'Olha', 'Orest', 'Otto', 'Paolo', 'Patrick', 'Paul', 'Pavlo', 'Peter', 'Petro', 'Philippe', 'Pierre', 'Pieter', 'Pietro', 'Raffaele', 'Raymond', 'Reinhold', 'Riccardo', 'Richard', 'Robert', 'Roberto', 'Robin', 'Roger', 'Roland', 'Rolf', 'Roman', 'Ronald', 'Ruben', 'Rudolf', 'Ryan', 'Salvatore', 'Sam', 'Sandrine', 'Scott', 'Sebastien', 'Sem', 'Sergio', 'Serhiy', 'Simone', 'Sophie', 'Stefan', 'Stefano', 'Stepan', 'Stephane', 'Stephen', 'Steven', 'Stijn', 'Sven', 'Svitlana', 'Taras', 'Tetyana', 'Teun', 'Thijs', 'Thomas', 'Tim', 'Timothy', 'Tom', 'Tommaso', 'Uliana', 'Umberto', 'Uwe', 'Vasyl', 'Viktor', 'Viktoriya', 'Vincenzo', 'Virginie', 'Volker', 'Volodymyr', 'Walter', 'Werner', 'Willem', 'Willi', 'William', 'Wolfgang', 'Wouter', 'Xavier', 'Yana', 'Yevhen', 'Yosyp', 'Yuliana', 'Yuliya', 'Yuriy', 'Yves'],
       assignedAliases: ['Jelle', 'Pieter', 'Jan'],
-      password: '',
-      formIsValid: true,
-      userEntryMode: 'login',
+
+      signInMessage: '',
+      signUpMessage: ''
     }
   },
 
   computed: {
-    submitButtonCaption() {
-      if (this.userEntryMode === 'login') {
-        return 'Login';
-      } else {
-        return 'Nieuwe aanmelding';
-      }
-    },
     switchModeButtonCaption() {
       if (this.userEntryMode === 'login') {
         return 'Ga naar nieuwe aanmelding';
@@ -102,29 +124,43 @@ export default {
         return 'Ga naar Login';
       }
     },
+    PINOK() {
+      return !isNaN(this.PIN) && this.PIN.length >= 4
+    },
+
+    aliasOK() {
+      if (this.userEntryMode === 'login') return this.alias.length > 0 && this.assignedAliases.includes(this.alias)
+      if (this.userEntryMode === 'signup') return this.alias.length > 0 && !this.assignedAliases.includes(this.alias)
+    }
   },
 
   methods: {
-    showSelectedAlias(selectedAlias) {
-      this.selectedAlias = selectedAlias
+    setSelectedAlias(selectedAlias) {
+      this.alias = selectedAlias
     },
 
-    async submitForm() {
-      console.log('submitForm')
-      this.formIsValid = true
-      if (
-        !this.matchGevonden ||
-        this.password.length < 6
-      ) {
-        this.formIsValid = false;
-        return
-      }
+    doSigninUser() {
+      this.signInMessage = 'alias OK= ' + this.aliasOK + ', PIN OK = ' + this.PINOK
+
+      // on success
+      this.isAuthenticated = true
+    },
+
+    doSignupUser() {
+      this.signUpMessage = 'alias OK= ' + this.aliasOK + ', PIN OK = ' + this.PINOK
+
+      // on success signin user
+      this.isAuthenticated = true
     },
 
     switchAuthMode() {
-      this.matchGevonden = false
-      this.password = ''
-      this.schuilNaam = ''
+      this.isAuthenticated = false
+      this.PIN = ''
+      this.PINOK = false
+      this.alias = ''
+      this.aliasOK = false,
+        this.signInMessage = ''
+      this.signUpMessage = ''
       if (this.userEntryMode === 'login') {
         this.userEntryMode = 'signup';
       } else {
@@ -152,45 +188,7 @@ export default {
         this.lastIndex = newIndex
       },
       deep: true
-    },
-
-    schuilNaam(val) {
-      if (this.userEntryMode === 'login') this.matchFound = this.assignedAliases.includes(val)
-      if (this.userEntryMode === 'signup') this.matchFound = (!this.assignedAliases.includes(val) && this.allAliases.includes(val))
     }
   }
 }
 </script>
-
-<style scoped>
-form {
-  margin: 1rem;
-  padding: 1rem;
-}
-
-.form-control {
-  margin: 0.5rem 0;
-}
-
-label {
-  font-weight: bold;
-  margin-bottom: 0.5rem;
-  display: block;
-}
-
-input,
-textarea {
-  display: block;
-  width: 100%;
-  font: inherit;
-  border: 1px solid #ccc;
-  padding: 0.15rem;
-}
-
-input:focus,
-textarea:focus {
-  border-color: #3d008d;
-  background-color: #faf6ff;
-  outline: none;
-}
-</style>
--- src/components/SelectAlias.vue
+++ src/components/SelectAlias.vue
@@ -1,155 +1,154 @@
 <template>
-  <v-card flat class="py-12">
-    <v-card-text>
-      <v-row align="center" justify="center">
-        <v-col cols="12">
-          <h2 class="text-center">
-            Kies een schuilnaam
-          </h2>
-        </v-col>
-        <v-btn-toggle v-model="selected[0]" mandatory>
-          <v-btn size="small">
-            {{randomNames[0]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[1]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[2]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[3]}}
-          </v-btn>
-        </v-btn-toggle>
-        <v-btn-toggle v-model="selected[1]" mandatory>
-          <v-btn size="small">
-            {{randomNames[4]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[5]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[6]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[7]}}
-          </v-btn>
-        </v-btn-toggle>
-        <v-btn-toggle v-model="selected[2]" mandatory>
-          <v-btn size="small">
-            {{randomNames[8]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[9]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[10]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[11]}}
-          </v-btn>
-        </v-btn-toggle>
-        <v-btn-toggle v-model="selected[3]" mandatory>
-          <v-btn size="small">
-            {{randomNames[12]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[13]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[14]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[15]}}
-          </v-btn>
-        </v-btn-toggle>
-        <v-btn-toggle v-model="selected[4]" mandatory>
-          <v-btn size="small">
-            {{randomNames[16]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[17]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[18]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[19]}}
-          </v-btn>
-        </v-btn-toggle>
-        <v-btn-toggle v-model="selected[5]" mandatory>
-          <v-btn size="small">
-            {{randomNames[20]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[21]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[22]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[23]}}
-          </v-btn>
-        </v-btn-toggle>
-        <v-btn-toggle v-model="selected[6]" mandatory>
-          <v-btn size="small">
-            {{randomNames[24]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[25]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[26]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[27]}}
-          </v-btn>
-        </v-btn-toggle>
-        <v-btn-toggle v-model="selected[7]" mandatory>
-          <v-btn size="small">
-            {{randomNames[28]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[29]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[30]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[31]}}
-          </v-btn>
-        </v-btn-toggle>
-        <v-btn-toggle v-model="selected[8]" mandatory>
-          <v-btn size="small">
-            {{randomNames[32]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[33]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[34]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[35]}}
-          </v-btn>
-        </v-btn-toggle>
-        <v-btn-toggle v-model="selected[9]" mandatory>
-          <v-btn size="small">
-            {{randomNames[36]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[37]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[38]}}
-          </v-btn>
-          <v-btn size="small">
-            {{randomNames[39]}}
-          </v-btn>
-        </v-btn-toggle>
-      </v-row>
-    </v-card-text>
-  </v-card>
+  <v-row>
+    <v-card flat>
+      <v-card-title class="text-h5">alias kiezen</v-card-title>
+      <v-card-subtitle>Scroll door alias namen en kies er een</v-card-subtitle>
+      <v-card-text>
+        <v-row align="center" justify="center">
+          <v-btn-toggle v-model="selected[0]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[0] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[1] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[2] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[3] }}
+            </v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle v-model="selected[1]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[4] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[5] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[6] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[7] }}
+            </v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle v-model="selected[2]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[8] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[9] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[10] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[11] }}
+            </v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle v-model="selected[3]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[12] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[13] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[14] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[15] }}
+            </v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle v-model="selected[4]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[16] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[17] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[18] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[19] }}
+            </v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle v-model="selected[5]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[20] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[21] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[22] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[23] }}
+            </v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle v-model="selected[6]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[24] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[25] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[26] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[27] }}
+            </v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle v-model="selected[7]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[28] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[29] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[30] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[31] }}
+            </v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle v-model="selected[8]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[32] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[33] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[34] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[35] }}
+            </v-btn>
+          </v-btn-toggle>
+          <v-btn-toggle v-model="selected[9]" mandatory>
+            <v-btn size="small">
+              {{ randomNames[36] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[37] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[38] }}
+            </v-btn>
+            <v-btn size="small">
+              {{ randomNames[39] }}
+            </v-btn>
+          </v-btn-toggle>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-row>
 </template>
 
 <script>
@@ -198,8 +197,4 @@ export default {
     }
   }
 }
-
-
-
-
 </script>
