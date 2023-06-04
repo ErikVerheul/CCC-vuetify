d7ec73878c7b4eadc839f2aa77d2024edb1b44e6
Erik Verheul <tech@verheulconsultants.nl>
2023-06-04 22:04:21 +0200

    Exposing firebase credentials is not a sucurity risk

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,8 @@ import { getDatabase } from 'firebase/database'
 import { ref as dbRef } from 'firebase/database'
 // ... other firebase imports
 
+
+// see https://jsmobiledev.com/article/hide-firebase-api/ on why it is not a security risk to expose these credentials
 export const firebaseApp = initializeApp({
   // your application settings
   apiKey: "AIzaSyBSLibHkymxkkXk2b1GdSUMfXPPW6ZyncA",
