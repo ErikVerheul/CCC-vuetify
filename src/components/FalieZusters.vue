aa253cf83d12c1c09fd7d8c2136bcddd0912f569
Erik Verheul <tech@verheulconsultants.nl>
2023-06-20 15:24:24 +0200

    Improve layout

--- src/components/FalieZusters.vue
+++ src/components/FalieZusters.vue
@@ -1,50 +1,50 @@
 <template>
-  <v-sheet>
-    <v-row class="d-flex justify-center align-center bg-grey-lighten-4">
+  <v-sheet width="window.innerWidth">
+    <v-row class="d-flex justify-center ">
       <v-col cols="auto">
-        <v-img :width="350" src="../assets/Faliezusterklooster.png"></v-img>
+        <v-img :width="300" src="../assets/Faliezusterklooster.png"></v-img>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="1">
       </v-col>
-      <v-col cols="9">
+      <v-col cols="10">
         <h3>Het voormalig Faliezusterklooster ligt vlakbij de Minderbroederkerk, de Pater Vinktoren, de Jeker en de oude stadsmuur. Waaraan danken de faliezusters hun naam?</h3>
       </v-col>
-      <v-col cols="2">
+      <v-col cols="1">
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="auto">
-        <v-btn>
+        <v-btn size="small">
           a.Omdat ze op hun lie kregen van het stadsbestuur
         </v-btn>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="auto">
-        <v-btn>
+        <v-btn size="small">
           b.Aan hun zwarte hoofdsluier
         </v-btn>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="auto">
-        <v-btn>
+        <v-btn size="small">
           c.Aan de naam van Sint Phalius
         </v-btn>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="auto">
-        <v-btn>
+        <v-btn size="small">
           d.Aan de naam van de oprichtster
         </v-btn>
       </v-col>
     </v-row>
-    <div class="py-8" />
-    <h4>Meer over kloosterleven in Maastricht Museum</h4>
     <div class="py-5" />
+    <h4>Meer over kloosterleven in Maastricht Museum</h4>
+    <div class="py-3" />
     <v-row class="justify-start">
       <v-col cols="auto">
         <v-btn flat prepend-icon="mdi-arrow-left" @click="emit('quit-quiz')">
