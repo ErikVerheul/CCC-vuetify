4e80c4afe0b3474c51dd024dfe1a7420cc3dc5f9
Erik Verheul <tech@verheulconsultants.nl>
2023-06-22 09:25:15 +0200

    Remove unused 'verhalen'

--- src/components/MaastrichtStories.vue
+++ src/components/MaastrichtStories.vue
@@ -31,22 +31,6 @@
     <v-container>
       <v-row dense>
         <v-col cols="12">
-          <v-card color="#385F73" theme="dark">
-            <v-card-title class="text-h5">
-              Een ander verhaal
-            </v-card-title>
-
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
-
-            <v-card-actions>
-              <v-btn variant="text">
-                Listen Now
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-
-        <v-col cols="12">
           <v-card color="#1F7087" theme="dark">
             <div class="d-flex flex-no-wrap justify-space-between">
               <div>
@@ -65,28 +49,6 @@
 
               <v-avatar class="ma-3" size="180" rounded="0">
                 <v-img src="../assets/Faliezusterklooster.png"></v-img>
-              </v-avatar>
-            </div>
-          </v-card>
-        </v-col>
-
-        <v-col cols="12">
-          <v-card color="#952175" theme="dark">
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title class="text-h5">
-                  Nog een verhaal
-                </v-card-title>
-
-                <v-card-subtitle>Ellie Goulding</v-card-subtitle>
-
-                <v-card-actions>
-                  <v-btn class="ms-2" icon="mdi-play" variant="text"></v-btn>
-                </v-card-actions>
-              </div>
-
-              <v-avatar class="ma-3" size="125" rounded="0">
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"></v-img>
               </v-avatar>
             </div>
           </v-card>
