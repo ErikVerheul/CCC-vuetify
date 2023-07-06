dcf3b40a9b9c0b05eade5b1f1a4c8b40ce59e8df
Erik Verheul <tech@verheulconsultants.nl>
2023-07-06 22:20:36 +0200

    Skip iterating alias if the page can hold more than avaiable

--- src/components/SelectAliasRow.vue
+++ src/components/SelectAliasRow.vue
@@ -1,7 +1,7 @@
 <template>
   <v-btn-toggle v-model="aliasButtonSelections[row]" mandatory>
     <template v-for="(num, index) in rowLength">
-      <template v-if="!aliasesInUse.includes(randomNames[indexOffset + index])">
+      <template v-if="!nameIsInUse(index)">
         <v-btn size="small">
           {{ randomNames[indexOffset + index] }}
         </v-btn>
@@ -21,7 +21,20 @@
 <script setup>
 import { computed } from 'vue'
 const props = defineProps(['aliasesInUse', 'randomNames', 'rowLength', 'row', 'aliasButtonSelections'])
+
 const indexOffset = computed(() => {
   return props.row * 4
 })
+
+function nameIsInUse(index) {
+  for (const el of props.aliasesInUse) {
+    const name = props.randomNames[indexOffset.value + index]
+    // skip if not enough names to fill the page
+    if (name === undefined) break
+    if (el.toUpperCase() === name.toUpperCase()) {
+      return true
+    }
+  }
+  return false
+}
 </script>
