e585faa5658a8b3d7cbda4675d9b8d77aab2f197
Erik Verheul <tech@verheulconsultants.nl>
2023-07-17 17:18:07 +0200

    Add Quiz maintenance

--- src/components/superadmin/SuperAdmin.vue
+++ src/components/superadmin/SuperAdmin.vue
@@ -6,13 +6,17 @@
           <v-list color="transparent">
             <v-list-item prepend-icon="mdi-view-dashboard" title="Schuilnamen onderhouden" @click="state.action = 'mAliases'"></v-list-item>
             <v-list-item prepend-icon="mdi-account" title="Schuilnaam info onderhouden" @click="state.action = 'maInfo'"></v-list-item>
+            <v-list-item prepend-icon="mdi-account" title="Quiz data onderhouden" @click="state.action = 'mQuiz'"></v-list-item>
             <v-list-item prepend-icon="mdi-account-box" title="Berichten maken en versturen" @click="state.action = 'mMess'"></v-list-item>
             <v-list-item prepend-icon="mdi-gavel" title="Andere gebruikers rechten geven" @click="state.action = 'mRoles'"></v-list-item>
           </v-list>
 
           <template v-slot:append>
             <div class="pa-2">
-              <v-btn block @click="router.replace({ path: '/' })">
+              <v-btn flat prepend-icon="mdi-arrow-left" @click="router.replace({ path: '/' })">
+                <template v-slot:prepend>
+                  <v-icon size="x-large" color="purple"></v-icon>
+                </template>
                 Terug
               </v-btn>
             </div>
@@ -21,30 +25,22 @@
       </v-row>
     </v-sheet>
 
-    <v-row>
-      <v-col v-if="state.action === 'mAliases'" cols="auto">
-        <MaintainAliases @m-done="state.action = 'menu'"></MaintainAliases>
-      </v-col>
-      <v-col v-if="state.action === 'maInfo'" cols="auto">
-        <MaintainAliasInfo @m-done="state.action = 'menu'"></MaintainAliasInfo>
-      </v-col>
-      <v-col v-if="state.action === 'mMess'" cols="auto">
-        <MaintainMessages @m-done="state.action = 'menu'"></MaintainMessages>
-      </v-col>
-      <v-col v-if="state.action === 'mRoles'" cols="auto">
-        <MaintainRoles @m-done="state.action = 'menu'"></MaintainRoles>
-      </v-col>
-    </v-row>
+    <MaintainAliases v-if="state.action === 'mAliases'" @m-done="state.action = 'menu'"></MaintainAliases>
+    <MaintainAliasInfo v-if="state.action === 'maInfo'" @m-done="state.action = 'menu'"></MaintainAliasInfo>
+    <MaintainQuizData v-if="state.action === 'mQuiz'" @m-done="state.action = 'menu'"></MaintainQuizData>
+    <MaintainMessages v-if="state.action === 'mMess'" @m-done="state.action = 'menu'"></MaintainMessages>
+    <MaintainRoles v-if="state.action === 'mRoles'" @m-done="state.action = 'menu'"></MaintainRoles>
   </v-container>
 </template>
 
 <script setup>
 import { reactive } from 'vue'
 import router from '@/router'
-import MaintainAliases from './MaintainAliases'
-import MaintainAliasInfo from './MaintainAliasInfo'
-import MaintainMessages from './MaintainMessages'
-import MaintainRoles from './MaintainRoles'
+import MaintainAliases from './MaintainAliases.vue'
+import MaintainAliasInfo from './MaintainAliasInfo.vue'
+import MaintainQuizData from './MaintainQuizData.vue'
+import MaintainMessages from './MaintainMessages.vue'
+import MaintainRoles from './MaintainRoles.vue'
 
 const state = reactive({
   action: 'menu'
