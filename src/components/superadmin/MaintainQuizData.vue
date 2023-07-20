2e7d687695bfe5e91db510611b1e125f5e236305
Erik Verheul <tech@verheulconsultants.nl>
2023-07-20 17:50:35 +0200

    2 lines per answer; url input added

--- src/components/superadmin/MaintainQuizData.vue
+++ src/components/superadmin/MaintainQuizData.vue
@@ -42,7 +42,15 @@
         </v-btn>
       </v-col>
     </v-row>
-    <v-text-field v-model="state.gameRules" label="Spelregels (optioneel)" />
+    <v-row>
+      <v-col cols="12">
+        <v-text-field v-model="state.gameRules" label="Spelregels (optioneel)" />
+      </v-col>
+      <v-col cols="6">
+        <v-text-field v-model="state.explanationUrl" :rules="state.urlRules" placeholder="https://speelmee.app/toelichting1"
+          label="URL naar toelichting (optioneel)" />
+      </v-col>
+    </v-row>
   </v-sheet>
 
   <v-row justify="space-around">
@@ -56,9 +64,9 @@
           <div v-html="state.content"></div>
         </v-row>
         <v-row no-gutters>
-          <v-list lines="one" density="compact">
+          <v-list lines="two" density="compact">
             <v-list-item v-for="(num, index) in state.questionsArray" :subtitle="composeQuestion(index)" @click="qAnswer(index)"
-              :style="{'background-color': bgColor}"></v-list-item>
+              :style="{ 'background-color': bgColor }"></v-list-item>
           </v-list>
         </v-row>
         <v-row v-if="state.gameRules !== ''" no-gutters>
@@ -76,9 +84,9 @@
           <div v-html="state.content"></div>
         </v-row>
         <v-row no-gutters>
-          <v-list lines="one" density="compact">
+          <v-list lines="two" density="compact">
             <v-list-item v-for="(num, index) in state.questionsArray" :subtitle="composeQuestion(index)" @click="qAnswer(index)"
-              :style="{'background-color': bgColor}"></v-list-item>
+              :style="{ 'background-color': bgColor }"></v-list-item>
           </v-list>
         </v-row>
         <v-row v-if="state.gameRules !== ''" no-gutters>
@@ -87,7 +95,7 @@
       </v-sheet>
     </v-col>
   </v-row>
-  <v-row v-if="countGoodAnswers() < 1">
+  <v-row v-if="countGoodAnswers() === 0">
     <p>Klik op de vragen in de preview om de goede antwoorden te selecteren.</p>
   </v-row>
 
@@ -102,7 +110,7 @@
     </v-col>
     <v-spacer></v-spacer>
     <v-col>
-      <v-btn :disabled="state.questionsArray.length < 1 || countGoodAnswers() < 1" flat append-icon="mdi-arrow-right" @click="saveQuiz">
+      <v-btn :disabled="state.questionsArray.length < 1 || countGoodAnswers() === 0" flat append-icon="mdi-arrow-right" @click="saveQuiz">
         Bewaar Quiz
         <template v-slot:append>
           <v-icon size="x-large" color="purple"></v-icon>
@@ -123,6 +131,10 @@ const state = reactive({
   questionsArray: [],
   gameRules: '',
   quizAnswers: {},
+  explanationUrl: undefined,
+  urlRules: [
+    (value) => isURL(value) || "URL is niet valide"
+  ]
 })
 
 // must be non-reactive
@@ -138,10 +150,20 @@ const editMode = computed(() => {
   } else return "add"
 })
 
+function isURL(str) {
+  let url;
+  try {
+    url = new URL(str);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:"
+}
+
 function composeQuestion(idx) {
   if (idx > 12) return "Fout: Meer dan 12 vragen?"
   const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
-  bgColor = 'white' 
+  bgColor = 'white'
   if (state.quizAnswers[idx]) {
     bgColor = 'aqua'
   }
@@ -181,23 +203,24 @@ function qForward() {
 
 function qAnswer(idx) {
   if (state.quizAnswers[idx]) {
-    state.quizAnswers[idx] = false   
+    state.quizAnswers[idx] = false
   } else {
     state.quizAnswers[idx] = true
   }
 }
 
 function countGoodAnswers() {
-  const anwwers = Object.keys(state.quizAnswers)
+  console.log('state.quizAnswers = ' + JSON.stringify(state.quizAnswers, null, 2))
+  const answers = Object.keys(state.quizAnswers)
   let count = 0
-  for (const el of anwwers) {
-    if (el) count++
+  for (const el of answers) {
+    if (state.quizAnswers[el]) count++
   }
   return count
 }
 
 function saveQuiz() {
-  console.log('countGoodAnswers = ' + countGoodAnswers)
+  console.log('countGoodAnswers = ' + countGoodAnswers())
 }
 
 </script> 
