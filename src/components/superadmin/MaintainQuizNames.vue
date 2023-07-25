23e18f4dc846727290e05afaf8623dd55365362d
Erik Verheul <tech@verheulconsultants.nl>
2023-07-25 22:52:17 +0200

    Feature 'Maintain quiz names' added

---
+++ src/components/superadmin/MaintainQuizNames.vue
@@ -0,0 +1,242 @@
+<template>
+  <v-card variant="tonal">
+    <v-card-title>Quiz namen onderhouden</v-card-title>
+    <v-row>
+      <v-col cols="3"></v-col>
+      <v-col cols="6">
+        <v-text-field v-model="state.quizNumberInput" label="Nieuw of te veranderen quiz nummer" :rules="state.numberRules" />
+      </v-col>
+      <v-col cols="3"></v-col>
+    </v-row>
+    <template v-if="state.quizNumberInput && numberOk()">
+      <v-row v-if="!QNumberExists()">
+        <v-col cols="3"></v-col>
+        <v-col cols="6">
+          <v-text-field v-model.trim="state.quizName" label="Nieuwe quiz naam" :rules="state.newNameRules" />
+        </v-col>
+        <v-col cols="3"></v-col>
+      </v-row>
+      <v-row v-else>
+        <v-col cols="3"></v-col>
+        <v-col cols="6">
+          <v-radio-group inline v-model="state.action">
+            <v-radio @change="changeMode" label="Veranderen" value="2"></v-radio>
+            <v-radio @change="changeMode" label="Verwijderen" value="3"></v-radio>
+          </v-radio-group>
+        </v-col>
+        <v-col cols="3"></v-col>
+
+        <v-col cols="3"></v-col>
+        <v-col cols="6">
+          <v-text-field v-if="state.action === '2'" v-model.trim="state.quizName" label="Verander quiz naam" :rules="state.newNameRules" />
+          <p v-if="state.action === '3'">Te verwijderen quiz naam: {{ state.quizName }}</p>
+        </v-col>
+        <v-col cols="3"></v-col>
+      </v-row>
+    </template>
+    <v-row>
+      <v-col>
+        <v-btn prepend-icon="mdi-arrow-left" @click="emit('m-done')">
+          <template v-slot:prepend>
+            <v-icon size="x-large" color="purple"></v-icon>
+          </template>
+          Terug
+        </v-btn>
+      </v-col>
+      <v-col v-if="state.saveSuccess === 1">
+        "{{ saveButtonText }}" is gelukt
+      </v-col>
+      <v-col v-if="!state.saveSuccess === 2">
+        "{{ saveButtonText }}" is mislukt
+      </v-col>
+      <v-col class="text-right">
+        <v-btn :disabled="!allowSave" :color="saveButtonColor" append-icon="mdi-arrow-right" @click="doSave">
+          {{ saveButtonText }}
+          <template v-slot:append>
+            <v-icon size="x-large" color="purple"></v-icon>
+          </template>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <h3>Quizzen overzicht (nummers hoeven niet aan te sluiten)</h3>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-list lines="one">
+          <v-list-item v-for="item in state.allQuizItems" :title="composeLine(item)"></v-list-item>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script setup>
+import { onBeforeMount, computed, reactive, watch } from 'vue'
+import { db, dbRef } from '../../firebase'
+import { ref, child, get, set, remove, update } from 'firebase/database'
+
+const emit = defineEmits(['m-done'])
+
+onBeforeMount(() => {
+  loadQuizes()
+})
+
+const state = reactive({
+  quizesObject: {},
+  action: '1',
+  allQuizNumbers: [],
+  allQuizItems: [],
+  quizNumbersInUse: [],
+  quizNumberInput: undefined,
+  quizName: '',
+  numberRules: [
+    value => {
+      if (value) return true
+
+      return 'Vul 1 of meer cijfers in.'
+    },
+    value => {
+      if (value.length >= 1) return true
+
+      return 'Vul minimaal 1 cijfer in.'
+    },
+
+    value => {
+      if (!isNaN(value)) return true
+
+      return 'Vul alleen cijfers in.'
+    },
+    value => {
+      if (value > 0) return true
+
+      return 'Moet een positief getal zijn.'
+    }
+  ],
+  newNameRules: [
+    value => {
+      if (value) return true
+
+      return 'Vul 2 of meer letters in.'
+    },
+    value => {
+      if (value.length >= 2) return true
+
+      return 'Vul minimaal 2 letters in.'
+    }
+  ],
+  saveSuccess: 0
+})
+
+function loadQuizes() {
+  // get all available quizes
+  get(child(dbRef, `/quizes/names/`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      state.quizesObject = snapshot.val()
+      state.allQuizNumbers = Object.keys(state.quizesObject)
+      state.allQuizItems = []
+      for (const nr of state.allQuizNumbers) {
+        state.allQuizItems.push({ [nr]: state.quizesObject[nr] })
+      }
+    } else {
+      console.log("No quizes available")
+    }
+  }).catch((error) => {
+    console.error('While reading all available quizes from database: error message = ' + error.message)
+  })
+}
+
+function numberOk() {
+  return !isNaN(state.quizNumberInput) && state.quizNumberInput > 0
+}
+
+function QNameOk() {
+  return state.quizName && state.quizName.length >= 2
+}
+
+function QNumberExists() {
+  return numberOk() && state.allQuizNumbers.includes(state.quizNumberInput)
+}
+
+function isQNumberInUse(number) {
+  return state.quizNumbersInUse.includes(number)
+}
+
+function changeMode() {
+  state.quizName = state.quizesObject[state.quizNumberInput].name
+  state.saveSuccess = 0
+}
+
+const allowSave = computed(() => {
+  if (state.action === '1') return numberOk() && QNameOk() && !QNumberExists()
+  if (state.action === '2') return QNameOk() && QNumberExists()
+  if (state.action === '3') return numberOk() && QNumberExists() && !isQNumberInUse(state.quizNumberInput)
+  return false
+})
+
+const saveButtonColor = computed(() => {
+  if (state.action === '3') return 'red'
+  return ''
+})
+
+const saveButtonText = computed(() => {
+  if (state.action === '1') return 'Voeg toe'
+  if (state.action === '2') return 'Sla op'
+  if (state.action === '3') return 'Verwijder'
+  return ''
+})
+
+function composeLine(item) {
+  const nr = Object.keys(item)[0]
+  return `${nr}) ${item[nr].name}`
+}
+
+function doSave() {
+  if (state.action === '1') {
+    // note: Using set() overwrites data at the specified location, including any child nodes.
+    set(ref(db, '/quizes/names/' + state.quizNumberInput), {
+      "name": state.quizName,
+      "creationDate": Number(new Date())
+    }).then(() => {
+      // refresh overall quiz data
+      loadQuizes()
+      state.saveSuccess = 1
+    }).catch((error) => {
+      state.saveSuccess = 2
+      console.error('The write failed, error message = ' + error.message)
+    })
+  }
+  if (state.action === '2') {
+    const updates = {}
+    updates[`quizes/names/${state.quizNumberInput}/name`] = state.quizName
+    update(dbRef, updates).then(() => {
+      // refresh overall quiz data
+      loadQuizes()
+      state.saveSuccess = 1
+    }).catch((error) => {
+      state.saveSuccess = 2
+      console.error('The name update failed, error message = ' + error.message)
+    })
+  }
+  if (state.action === '3') {
+    remove(child(dbRef, '/quizes/names/' + state.quizNumberInput)).then(() => {
+      // refresh overall quiz data
+      loadQuizes()
+      state.saveSuccess = 1
+    }).catch((error) => {
+      state.saveSuccess = 2
+      console.error('The remove failed, error message = ' + error.message)
+    })
+  }
+}
+
+watch(() => state.quizNumberInput, () => {
+  state.quizName = ''
+  state.action = '1'
+  state.saveSuccess = 0
+})
+
+
+</script>
--- src/components/superadmin/SuperAdmin.vue
+++ src/components/superadmin/SuperAdmin.vue
@@ -5,7 +5,8 @@
         <v-navigation-drawer width="100%" class="bg-deep-purple" theme="dark" permanent>
           <v-list color="transparent">
             <v-list-item prepend-icon="mdi-view-dashboard" title="Schuilnamen onderhouden" @click="state.action = 'mAliases'"></v-list-item>
-            <v-list-item prepend-icon="mdi-account" title="Quiz data onderhouden" @click="state.action = 'mQuiz'"></v-list-item>
+            <v-list-item prepend-icon="mdi-account" title="Quiz namen onderhouden" @click="state.action = 'mQuizNames'"></v-list-item>
+            <v-list-item prepend-icon="mdi-account" title="Quiz vragen onderhouden" @click="state.action = 'mQuiz'"></v-list-item>
             <v-list-item prepend-icon="mdi-account-box" title="Berichten maken en versturen" @click="state.action = 'mMess'"></v-list-item>
             <v-list-item prepend-icon="mdi-gavel" title="Andere gebruikers rechten geven" @click="state.action = 'mRoles'"></v-list-item>
           </v-list>
@@ -25,6 +26,7 @@
     </v-sheet>
 
     <MaintainAliases v-if="state.action === 'mAliases'" @m-done="state.action = 'menu'"></MaintainAliases>
+    <MaintainQuizNames v-if="state.action === 'mQuizNames'" @m-done="state.action = 'menu'"></MaintainQuizNames>
     <MaintainQuizData v-if="state.action === 'mQuiz'" @m-done="state.action = 'menu'"></MaintainQuizData>
     <MaintainMessages v-if="state.action === 'mMess'" @m-done="state.action = 'menu'"></MaintainMessages>
     <MaintainRoles v-if="state.action === 'mRoles'" @m-done="state.action = 'menu'"></MaintainRoles>
@@ -35,6 +37,7 @@
 import { reactive } from 'vue'
 import router from '@/router'
 import MaintainAliases from './MaintainAliases.vue'
+import MaintainQuizNames from './MaintainQuizNames.vue'
 import MaintainQuizData from './MaintainQuizData.vue'
 import MaintainMessages from './MaintainMessages.vue'
 import MaintainRoles from './MaintainRoles.vue'
