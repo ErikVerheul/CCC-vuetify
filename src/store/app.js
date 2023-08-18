e0ca71ccc551f3acd6d24b6a76f746c92c0beaa2
Erik Verheul <tech@verheulconsultants.nl>
2023-08-18 16:57:50 +0200

    Allow 60px for browser address bar

--- src/store/app.js
+++ src/store/app.js
@@ -4,8 +4,8 @@ import { defineStore } from 'pinia'
 export const useAppStore = defineStore('app', {
   state: () => ({
     screenWidth: window.screen.availWidth > 600 ? 600 : window.screen.availWidth,
-    // allow 120 px for unknown space usage
-    screenHeight: window.screen.availHeight - 120,
+    // allow 120 px for unknown space usage like browser adres bar
+    screenHeight: window.screen.availHeight - 180,
     backContinueHeight: 60,
     firebaseUser: undefined,
     userData: {},
