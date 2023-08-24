8a2db410d54c427842be41d62d17401de8f63b06
Erik Verheul <tech@verheulconsultants.nl>
2023-08-24 15:19:55 +0200

    Typo

--- src/components/SigninUser.vue
+++ src/components/SigninUser.vue
@@ -18,7 +18,7 @@
         <v-card-title>Login met schuilnaam en PIN code</v-card-title>
         <v-autocomplete v-model="store.userData.alias" :items="props.aliasesInUseInclAdmin" label="Uw schuilnaam" />        
         <v-text-field v-model.trim="store.userData.PIN" label="PIN" :rules="state.pinRules" />
-        // check for store.userData.alias not null
+        <!-- check for store.userData.alias not null -->
         <v-btn class="my-6" v-if="store.userData.alias && PINOK" type="submit" color="purple" @click='doSigninUser' rounded="l"
           size="large">Login</v-btn>
         <template v-if="state.loginErrorMsg !== undefined">
