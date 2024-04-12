8625e5faf99139205ef0486763ef0e6b141d32f5
Erik Verheul <erik@verheulconsultants.nl>
2024-04-12 21:39:10 +0200

    Fix path name

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
         component: PrivacyView
       },
       {
-        path: '/superadmin',
+        path: '/SuperAdmin',
         name: 'SuperAdmin',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
