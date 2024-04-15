7ce2336c85a915ca0023a942b341406fb250725d
Erik Verheul <erik@verheulconsultants.nl>
2024-04-15 16:30:47 +0200

    Remove text implicating there is a logging

--- src/components/ReportFbError.vue
+++ src/components/ReportFbError.vue
@@ -4,7 +4,6 @@
     <v-card-text>
       <p><b>De database geeft de volgende melding:</b> {{ props.firebaseError.message }}</p>
       <p><b>Tijdens/bij:</b> {{ props.fbErrorContext }}</p>
-      <p><b>De fout wordt gelogd. De app ontwikkelaar zal hier z.s.m. naar kijken</b></p>
     </v-card-text>
     <v-card-actions>
       <v-row>
@@ -22,10 +21,8 @@
 </template>
 
 <script setup>
-import { reactive, watch } from 'vue'
 import { useAppStore } from '../store/app.js'
-import { db, dbRef } from '../firebase'
-import { ref, child, get, set, remove, update } from 'firebase/database'
+
 const store = useAppStore()
 const emit = defineEmits(['return-to'])
 const props = defineProps({
