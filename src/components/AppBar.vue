b0d57392b7fb120c40125dbf5f5254a3e8755c28
Erik Verheul <erik@verheulconsultants.nl>
2024-04-28 16:20:53 +0200

    Allow admin functions on large screen only.

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar density="compact" color="purple">
       <v-app-bar-title class="flex text-center">Hoe goed ken je Maastricht?</v-app-bar-title>
-      <v-btn v-if="props.isAuthenticated && store.userData.alias === 'admin'" size="small" variant="outlined" @click="doAdmin">Admin</v-btn>
+      <v-btn v-if="store.largeScreenDetected && props.isAuthenticated && store.userData.alias === 'admin'" size="small" variant="outlined" @click="doAdmin">Admin</v-btn>
     <v-btn icon id="menu-activator">
       <v-icon>mdi-dots-vertical</v-icon>
     </v-btn>
