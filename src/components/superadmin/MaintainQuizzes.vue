8b32cab6016a43b13c8c0c3d55b9065d79f00375
Erik Verheul <erik@verheulconsultants.nl>
2024-08-26 12:40:42 +0200

    Refactor with autocomplete for existing quizzes

--- src/components/superadmin/MaintainQuizzes.vue
+++ src/components/superadmin/MaintainQuizzes.vue
@@ -1,77 +1,98 @@
 <template>
+  <p>state.newQuizNumberInput = {{ state.newQuizNumberInput }}</p>
+  <p>state.quizNumberInput = {{ state.quizNumberInput }}</p>
+  <p>state.actionWeek = {{ state.actionWeek }}</p>
+  <p>state.action = {{ state.action }}</p>
+  <p>quizzesAssignedAlready = {{ JSON.stringify(quizzesAssignedAlready) }}</p>
+  <p>editNew = {{ editNew }}</p>
+  <p>editExisting = {{ editExisting }}</p>
+  <p>saveButtonText = {{ saveButtonText }}</p>
   <v-card>
-    <v-card-title>Quiz meta data onderhouden</v-card-title>
+    <v-card-title class="text-center">Quiz meta data onderhouden</v-card-title>
     <v-row>
       <v-col cols="3"></v-col>
       <v-col cols="6">
-        <v-text-field v-model="state.quizNumberInput" label="Nieuw of te veranderen quiz nummer" :rules="state.numberRules" />
+        <v-text-field v-if="editNew" v-model="state.newQuizNumberInput" label="Nieuw quiz nummer" :rules="state.newNumberRules" />
+        <v-autocomplete v-if="editExisting" v-model="state.quizNumberInput" item-value="key" :items="state.allQuizItems" label="Te veranderen quiz" />
       </v-col>
       <v-col cols="3"></v-col>
     </v-row>
-    <template v-if="state.quizNumberInput && QNumberOk()">
-      <v-row v-if="!QNumberExists()">
+    <template v-if="state.newQuizNumberInput">
+      <v-row>
         <v-col cols="3"></v-col>
         <v-col cols="6">
           <v-text-field v-model.trim="state.quizTitle" label="Nieuwe quiz naam" :rules="state.newNameRules" />
         </v-col>
         <v-col cols="3"></v-col>
       </v-row>
-      <template v-else>
-        <v-row>
+    </template>
+    <template v-if="state.quizNumberInput">
+      <v-row>
+        <v-col cols="3"></v-col>
+        <v-col cols="6">
+          <v-radio-group inline v-model="state.action">
+            <v-radio :disabled="state.quizNumberInput === '0'" @change="changeMode" label="Veranderen" value="2"></v-radio>
+            <v-radio @change="changeMode" label="Maak quiz leeg" value="3"></v-radio>
+            <v-radio @change="createQuestionsList(state.quizNumberInput)" label="Toon vragen" value="4"></v-radio>
+          </v-radio-group>
+        </v-col>
+        <v-col cols="3"></v-col>
+
+        <v-col cols="3"></v-col>
+        <v-col cols="6">
+          <v-text-field v-if="state.action === '2'" v-model.trim="state.quizTitle" label="Verander quiz naam" :rules="state.newNameRules" />
+          <p v-if="state.action === '3'">Leeg te maken quiz naam: {{ state.quizTitle }}</p>
+        </v-col>
+        <v-col cols="3"></v-col>
+        <template v-if="state.action === '2'">
           <v-col cols="3"></v-col>
           <v-col cols="6">
-            <v-radio-group inline v-model="state.action">
-              <v-radio :disabled="state.quizNumberInput === '0'" @change="changeMode" label="Veranderen" value="2"></v-radio>
-              <v-radio @change="changeMode" label="Maak quiz leeg" value="3"></v-radio>
-              <v-radio @change="createQuestionsList(state.quizNumberInput)" label="Toon vragen" value="4"></v-radio>
-            </v-radio-group>
+            <v-text-field v-model="state.actionYear" label="Actie jaar" :rules="state.yearRules" />
           </v-col>
           <v-col cols="3"></v-col>
 
           <v-col cols="3"></v-col>
           <v-col cols="6">
-            <v-text-field v-if="state.action === '2'" v-model.trim="state.quizTitle" label="Verander quiz naam" :rules="state.newNameRules" />
-            <p v-if="state.action === '3'">Leeg te maken quiz naam: {{ state.quizTitle }}</p>
+            <v-text-field v-model="state.actionWeek" label="Actie week" :rules="state.weekRules" />
           </v-col>
           <v-col cols="3"></v-col>
-          <template v-if="state.action === '2'">
+          <template v-if="actionWeekInUse">
             <v-col cols="3"></v-col>
             <v-col cols="6">
-              <v-text-field v-model="state.actionYear" label="Actie jaar" :rules="state.yearRules" />
+              <p class="color-red">Er is al een quiz aan dit actie jaar en deze week gekoppeld</p>
             </v-col>
-            <v-col cols="3"></v-col>
-
-            <v-col cols="3"></v-col>
-            <v-col cols="6">
-              <v-text-field v-model="state.actionWeek" label="Actie week" :rules="state.weekRules" />
+            <v-col cols="3">
+              <v-btn @click="state.checkMultipleAssignments = true">Welke quiz?</v-btn>
             </v-col>
-            <v-col cols="3"></v-col>
-            <template v-if="actionWeekInUse">
-              <v-col cols="3"></v-col>
-              <v-col cols="6">
-                <p class="color-red">Er is al een quiz aan dit actie jaar en deze week gekoppeld</p>
-              </v-col>
-              <v-col cols="3">
-                <v-btn @click="state.checkMultipleAssignments = true">Welke quiz?</v-btn>
-              </v-col>
-            </template>
           </template>
-        </v-row>
-      </template>
-      <v-row v-if="!QNumberExists()">
-        <v-col cols="6">
-          <v-text-field v-model="state.actionYear" label="Actie jaar" :rules="state.yearRules" />
-        </v-col>
-        <v-col cols="6">
-          <v-text-field v-model="state.actionWeek" label="Actie week" :rules="state.weekRules" />
-        </v-col>
-      </v-row>
-      <v-row v-if="state.resetCount > 0">
-        <v-col cols="12">
-          <h4>{{ state.resetCount }} referenties naar deze quiz in de quiz vragen zijn verwijderd.</h4>
-        </v-col>
+        </template>
       </v-row>
     </template>
+    <v-row v-if="state.newQuizNumberInput">
+      <v-col cols="6">
+        <v-text-field v-model="state.actionYear" label="Actie jaar" :rules="state.yearRules" />
+      </v-col>
+      <v-col cols="6">
+        <v-text-field v-model="state.actionWeek" label="Actie week" :rules="state.weekRules" />
+      </v-col>
+    </v-row>
+    <v-row v-if="state.resetCount > 0">
+      <v-col cols="12">
+        <h4>{{ state.resetCount }} referenties naar deze quiz in de quiz vragen zijn verwijderd.</h4>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="5" class="text-left">
+        <v-btn @click="doCancel">Cancel</v-btn>
+      </v-col>
+      <v-col cols="4" v-if="successText">"{{ successText }}"</v-col>
+      <v-col v-else cols="4"></v-col>
+      <v-col cols="3" v-if="state.action && state.action != '4'" class="text-right">
+        <v-btn :disabled="!allowSave" :color="saveButtonColor" @click="doAction">
+          {{ saveButtonText }}
+        </v-btn>
+      </v-col>
+    </v-row>
     <v-divider class="mb-5"></v-divider>
     <v-row>
       <v-col>
@@ -81,32 +102,6 @@
           </template>
           Terug
         </v-btn>
-      </v-col>
-      <v-col v-if="state.saveSuccess === 1">
-        "{{ saveButtonText }}" is gelukt
-      </v-col>
-      <v-col v-if="!state.saveSuccess === 2">
-        "{{ saveButtonText }}" is mislukt
-      </v-col>
-      <v-col v-if="state.action != '4'" class="text-right">
-        <v-btn :disabled="!allowSave" :color="saveButtonColor" append-icon="mdi-arrow-right" @click="doAction">
-          {{ saveButtonText }}
-          <template v-slot:append>
-            <v-icon size="x-large" color="purple"></v-icon>
-          </template>
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <h3>Quizzen overzicht (nummers hoeven niet aan te sluiten)</h3>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <v-list lines="one">
-          <v-list-item v-for="item in state.allQuizItems" :title="composeLine(item)"></v-list-item>
-        </v-list>
       </v-col>
     </v-row>
   </v-card>
@@ -168,20 +163,20 @@ const store = useAppStore()
 const emit = defineEmits(['m-done'])
 
 onBeforeMount(() => {
-  loadQuizes()
+  resetAndloadQuizes()
 })
 
 const state = reactive({
   allQuizNumbers: [],
   allQuizItems: [],
-
+  newQuizNumberInput: undefined,
   quizNumberInput: undefined,
-  action: '1',
+  action: undefined,
   quizTitle: '',
-  actionYear: '',
-  actionWeek: '',
+  actionYear: undefined,
+  actionWeek: undefined,
   creationDate: undefined,
-  numberRules: [
+  newNumberRules: [
     value => {
       if (value) return true
 
@@ -202,6 +197,11 @@ const state = reactive({
       if (value >= 0) return true
 
       return 'Moet 0 of een positief getal zijn.'
+    },
+    value => {
+      if (!QNumberExists(value)) return true
+
+      return 'Dit quiz nummer is al in gebruik'
     }
   ],
   newNameRules: [
@@ -258,13 +258,30 @@ const state = reactive({
   showQuestions: false
 })
 
+const editExisting = computed(() => {
+  return (state.newQuizNumberInput === '' && state.quizNumberInput === '') || (state.quizNumberInput !== '' && state.newQuizNumberInput === '')
+})
+
+const editNew = computed(() => {
+  return (state.newQuizNumberInput === '' && state.quizNumberInput === '') || (state.newQuizNumberInput !== '' && state.quizNumberInput === '')
+})
+
 function actionSettingsOk() {
-  return state.actionYear === '' && state.actionWeek === '' ||
+  return state.actionYear && state.actionWeek &&
     (!isNaN(state.actionYear) && !isNaN(state.actionWeek) &&
       state.actionYear >= 2023 && state.actionYear < 2100 && state.actionWeek >= 0 && state.actionWeek <= 53)
 }
 
-function loadQuizes() {
+function resetAndloadQuizes() {
+  // reset input
+  state.newQuizNumberInput = ''
+  state.quizNumberInput = ''
+  state.action = undefined
+  state.quizTitle = ''
+  state.actionYear = undefined
+  state.actionWeek = undefined
+  state.creationDate = undefined
+  state.saveSuccess = 0
   // get all available quizzes
   get(child(dbRef, `/quizzes/metaData/`)).then((snapshot) => {
     if (snapshot.exists()) {
@@ -273,7 +290,7 @@ function loadQuizes() {
       state.allQuizNumbers = Object.keys(store.metaObject)
       state.allQuizItems = []
       for (const nr of state.allQuizNumbers) {
-        state.allQuizItems.push({ [nr]: store.metaObject[nr] })
+        state.allQuizItems.push({ key: nr, title: `${nr}) ${store.metaObject[nr].title}` })
       }
     } else {
       console.log("No quizzes available")
@@ -329,19 +346,20 @@ function createQuestionsList(quizNr) {
   })
 }
 
-function QNumberOk() {
-  return !isNaN(state.quizNumberInput) && state.quizNumberInput >= 0
+function QNumberOk(number) {
+  return !isNaN(number) && number >= 0
 }
 
 function QNameOk() {
   return state.quizTitle && state.quizTitle.length >= 2
 }
 
-function QNumberExists() {
-  return QNumberOk() && state.allQuizNumbers.includes(state.quizNumberInput)
+function QNumberExists(number) {
+  return QNumberOk(number) && state.allQuizNumbers.includes(number)
 }
 
 function changeMode() {
+  state.showQuestions = false
   state.quizTitle = store.metaObject[state.quizNumberInput].title
   state.actionYear = store.metaObject[state.quizNumberInput].actionYear
   state.actionWeek = store.metaObject[state.quizNumberInput].actionWeek
@@ -351,9 +369,9 @@ function changeMode() {
 }
 
 const allowSave = computed(() => {
-  if (state.action === '1') return actionSettingsOk() && QNumberOk() && QNameOk() && !QNumberExists()
-  if (state.action === '2') return !actionWeekInUse.value && actionSettingsOk() && QNameOk() && QNumberExists()
-  if (state.action === '3') return QNumberOk() && QNumberExists()
+  if (state.action === '1') return actionSettingsOk() && QNameOk() && !QNumberExists(state.newQuizNumberInput)
+  if (state.action === '2') return !actionWeekInUse.value && actionSettingsOk() && QNameOk() && QNumberExists(state.quizNumberInput)
+  if (state.action === '3') return QNumberOk(state.quizNumberInput) && QNumberExists(state.quizNumberInput)
   return false
 })
 
@@ -368,6 +386,13 @@ const saveButtonText = computed(() => {
   if (state.action === '3') return 'Maak leeg'
   if (state.action === '4') return 'TEST'
   return ''
+})
+
+const successText = computed(() => {
+  if (saveButtonText.value === '') return undefined
+  if (state.saveSuccess === 1) return saveButtonText.value + ' is gelukt'
+  if (state.saveSuccess === 2) return saveButtonText.value + ' is mislukt'
+  return undefined
 })
 
 const actionWeekInUse = computed(() => {
@@ -394,17 +419,22 @@ function composeQuestionLine(item) {
   return `Vraagnummer: ${item.question} Titel: ${item.title}`
 }
 
+function doCancel() {
+  // refresh overall quiz data
+  resetAndloadQuizes()
+}
+
 function doAction() {
   if (state.action === '1') {
     // note: Using set() overwrites data at the specified location, including any child nodes.
-    set(ref(db, '/quizzes/metaData/' + state.quizNumberInput), {
+    set(ref(db, '/quizzes/metaData/' + state.newQuizNumberInput), {
       "title": state.quizTitle,
       "actionYear": state.actionYear,
       "actionWeek": state.actionWeek,
       "creationDate": Number(new Date())
     }).then(() => {
       // refresh overall quiz data
-      loadQuizes()
+      resetAndloadQuizes()
       state.saveSuccess = 1
     }).catch((error) => {
       state.saveSuccess = 2
@@ -422,7 +452,7 @@ function doAction() {
     updates[`quizzes/metaData/${state.quizNumberInput}`] = newQuizObject
     update(dbRef, updates).then(() => {
       // refresh overall quiz data
-      loadQuizes()
+      resetAndloadQuizes()
       state.saveSuccess = 1
     }).catch((error) => {
       state.saveSuccess = 2
@@ -443,7 +473,7 @@ function doAction() {
       // remove references to the removed quiz
       removeQuizRefs(state.quizNumberInput)
       // refresh overall quiz data
-      loadQuizes()
+      resetAndloadQuizes()
       state.saveSuccess = 1
     }).catch((error) => {
       state.saveSuccess = 2
@@ -452,11 +482,12 @@ function doAction() {
   }
 }
 
-watch(() => state.quizNumberInput, () => {
-  state.quizTitle = ''
-  state.action = '1'
-  state.actionYear = ''
-  state.actionWeek = ''
+watch(() => editNew.value, () => {
+  if (editNew.value) { state.action = '1' } else state.action = undefined
+})
+
+watch(() => editExisting.value, () => {
+  if (editExisting.value) { state.action = undefined } else state.action = '1'
 })
 </script>
 
